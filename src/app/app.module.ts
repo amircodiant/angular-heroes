f6b33d53557efe4a1309283c1f1f3cd004b113e3
amircodiant <amira@neuronsolutions.com>
2017-10-25 19:56:26 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms'; /* this is require if you are using ngModel */
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroService } from './hero.service';
 
+
+import { AppRoutingModule }     from './app-routing.module';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, // import FormsModule before binding with [(ngModel)]
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
